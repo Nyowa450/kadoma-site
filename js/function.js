3fce3b7a736faf017ad18e7d3ea46068607969ad
kadoma56 <ninyo.lifehack3@gmail.com>
2022-06-05 04:42:39 +0900

    Nav-menu,css-import,congiure some setttings

--- js/function.js
+++ js/function.js
@@ -1,11 +1,11 @@
-$(".menuIco").click(function () {//ボタンがクリックされたら
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+$(".menuIco").click(function () {
+	$(this).toggleClass('active');
     $(".header-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
     $(".nav-circle").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
 });
 
-$(".header-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".menuIco").removeClass('active');//ボタンの activeクラスを除去し
+$(".header-nav").click(function () {
+    $(".menuIco").removeClass('active');//activeクラスを除去
     $(".header-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
-    $("nav-circle").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+    $(".nav-circle").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
